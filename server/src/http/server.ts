a67ec03e51194f3e541291fe4eb9bd178e3bb30a
Bruno H. Homem <brunohhomem@gmail.com>
2024-12-19 18:49:07 -0300

    feat: add post(createGoal)

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,37 @@
 import fastify from 'fastify'
+import { CreateGoal } from '../functions/create-goal'
+import z from 'zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7)
+      })
+    }
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await CreateGoal({
+      title,
+      desiredWeeklyFrequency
+    })
+  }
+)
 
 app
   .listen({
