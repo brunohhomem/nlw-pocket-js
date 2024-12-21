be5d7d2b30d16992cdbb4051e83cba993ed043d8
Bruno H. Homem <brunohhomem@gmail.com>
2024-12-21 15:28:58 -0300

    refactor: routes

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -4,10 +4,9 @@ import {
   validatorCompiler,
   type ZodTypeProvider
 } from 'fastify-type-provider-zod'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 import { createGoalRoute } from './routes/create-goal'
-import z from 'zod'
-import { createGoalCompletion } from '../functions/create-goal-completion'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,30 +14,9 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.get('/pending-goals', async () => {
-  const { pendingGoals } = await getWeekPendingGoals()
-
-  return { pendingGoals }
-})
-
-app.post(
-  '/completions',
-  {
-    schema: {
-      body: z.object({
-        goalId: z.string()
-      })
-    }
-  },
-  async request => {
-    const { goalId } = request.body
-    await createGoalCompletion({
-      goalId
-    })
-  }
-)
-
 app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
 
 app
   .listen({
