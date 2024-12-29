730853508ca4f77b3c434dc0c2f693fcdc3aebe4
Bruno H. Homem <brunohhomem@gmail.com>
2024-12-29 15:41:29 -0300

    feat: fetch summary data tanstack

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,40 +1,19 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
-import { useEffect, useState } from 'react'
 import { Summary } from './components/summary'
 import { EmptyGoals } from './components/empty-goals'
-
-type SummaryResponse = {
-  completed: number
-  total: number
-  goalsPerDay: Record<
-    string,
-    {
-      id: string
-      title: string
-      completedAt: string
-    }[]
-  >
-}
+import { useQuery } from '@tanstack/react-query'
+import { GetSummary } from './http/get-summary'
 
 export function App() {
-  const [summary, setSummary] = useState<SummaryResponse | null>(null)
-
-  useEffect(() => {
-    fetch('http://localhost:3333/summary')
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setSummary(data.summary)
-      })
-  }, [])
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: GetSummary
+  })
 
   return (
     <Dialog>
-      {summary && summary.total > 0 ? <Summary /> : <EmptyGoals />}
-
-      <pre>{JSON.stringify(summary, null, 2)}</pre>
+      {data && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
