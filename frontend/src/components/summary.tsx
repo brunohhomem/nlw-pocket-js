e6b6b9fead15b1afe94021ae22289d3a45d050fb
Bruno H. Homem <brunohhomem@gmail.com>
2024-12-30 18:22:12 -0300

    feat: add pending goals mockup

--- frontend/src/components/summary.tsx
+++ frontend/src/components/summary.tsx
@@ -4,11 +4,11 @@ import { DialogTrigger } from './ui/dialog'
 import InOrbitIcon from './ui/in-orbit-icon'
 import { Progress, ProgressIndicator } from './ui/progress-bar'
 import { Separator } from './ui/separator'
-import { OutlineButton } from './ui/outline-button'
 import { useQuery } from '@tanstack/react-query'
 import { GetSummary } from '../http/get-summary'
 import dayjs from 'dayjs'
 import ptBR from 'dayjs/locale/pt-BR'
+import { PendingGoals } from './pending-goals'
 
 dayjs.locale(ptBR)
 
@@ -63,22 +63,7 @@ export function Summary() {
 
       <Separator />
 
-      <div className="flex flex-wrap gap-3">
-        <OutlineButton>
-          <Plus className="size-4 text-zinc-600" />
-          Meditar
-        </OutlineButton>
-
-        <OutlineButton>
-          <Plus className="size-4 text-zinc-600" />
-          Nadar
-        </OutlineButton>
-
-        <OutlineButton>
-          <Plus className="size-4 text-zinc-600" />
-          Praticar Exercícios
-        </OutlineButton>
-      </div>
+      <PendingGoals />
 
       <div className="flex flex-col gap-6">
         <h2 className="text-xl font-medium">Sua semana:</h2>
